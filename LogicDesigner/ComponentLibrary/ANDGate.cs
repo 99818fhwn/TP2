0df58e1262a88900536533d61b646979173bb2d1
Fabian Weisser <99818@fhwn.ac.at>
2019-01-15 11:38:45 +0100

    Shared.dll added ComponentLoader AndComponent

---
+++ LogicDesigner/ComponentLibrary/ANDGate.cs
@@ -0,0 +1,57 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComponentLibrary
+{
+    public class ANDGate : IDisplayableNode
+    {
+        public ANDGate()
+        {
+            this.Inputs = new List<IPin>();
+            this.Outputs = new List<IPin>();
+            this.Label = "AND";
+            this.Description = "If all inputs are true, the output is true";
+            this.Picture = ;
+        }
+
+        public ICollection<IPin> Inputs
+        {
+            get;
+        }
+
+        public ICollection<IPin> Outputs
+        {
+            get;
+        }
+
+        public string Label
+        {
+            get;
+        }
+
+        public string Description
+        {
+            get;
+        }
+
+        public System.Drawing.Bitmap Picture
+        {
+            get;
+        }
+
+        public event EventHandler PictureChanged;
+
+        public void Execute()
+        {
+            if (true)
+            {
+
+            }
+        }
+    }
+}
---
+++ LogicDesigner/LogicDesigner/Model/ComponentLoader.cs
@@ -0,0 +1,44 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicDesigner.Model
+{
+    public class ComponentLoader
+    {
+        public List<IDisplayableNode> GetNode(string[] paths)
+        {
+            var nodes = new List<IDisplayableNode>();
+
+            foreach (var path in paths)
+            {
+                if (File.Exists(path))
+                {
+                    try
+                    {
+                        var asm = Assembly.LoadFrom(path);
+
+                        var asmItems = asm.GetExportedTypes().Where(a => a.GetInterfaces().Any(b => b.IsGenericType == true && b.GetGenericTypeDefinition() == typeof(IDisplayableNode)));
+
+                        foreach (var item in asmItems)
+                        {
+                            var instance = (IDisplayableNode)Activator.CreateInstance(item);
+                            nodes.Add(instance);
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        throw new Exception(e.Message);
+                    }
+                }
+            }
+
+            return nodes;
+        }
+    }
+}
