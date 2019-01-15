32cf24859d758085521d5ea60c67951f7efd2a86
Stephan Pillhofer <103042@fhwn.ac.at>
2019-01-15 14:10:02 +0100

    First drag and drop stuff

--- LogicDesigner/LogicDesigner/View/MainWindow.xaml.cs
+++ LogicDesigner/LogicDesigner/View/MainWindow.xaml.cs
@@ -23,13 +23,87 @@
     public partial class MainWindow : Window
     {
         /// <summary>
+        /// If a component is beeing dragged.
+        /// </summary>
+        private bool isMoving;
+
+        /// <summary>
+        /// The component position.
+        /// </summary>
+        private Point? componentPosition;
+
+        /// <summary>
+        /// The delta of the clicked x position on the component and the mouse x position.
+        /// </summary>
+        private double deltaX;
+
+        /// <summary>
+        /// The delta of the clicked y position on the component and the mouse y position.
+        /// </summary>
+        private double deltaY;
+
+        /// <summary>
+        /// The translated transform.
+        /// </summary>
+        private TranslateTransform translateTransform;
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="MainWindow"/> class.
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+        }
 
-            // New comment
+        /// <summary>
+        /// Called when the button is pressed down.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MouseButtonEventArgs"/> instance containing the event data.</param>
+        private void ComponentMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (this.componentPosition == null)
+            {
+                this.componentPosition = SampleComponent.TransformToAncestor(ComponentGrid).Transform(new Point(0, 0));
+            }
+
+            Point mousePosition = Mouse.GetPosition(ComponentGrid);
+
+            this.deltaX = mousePosition.X - this.componentPosition.Value.X;
+            this.deltaY = mousePosition.Y - this.componentPosition.Value.Y;
+
+            this.isMoving = true;
+        }
+
+        /// <summary>
+        /// Called when the button is pressed up.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MouseButtonEventArgs"/> instance containing the event data.</param>
+        private void ComponentMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            this.translateTransform = SampleComponent.RenderTransform as TranslateTransform;
+            isMoving = false;
+        }
+
+        /// <summary>
+        /// Called when the button is moved.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+        private void ComponentMouseMove(object sender, MouseEventArgs e)
+        {
+            if (!this.isMoving)
+            {
+                return;
+            }
+
+            var mousePosition = Mouse.GetPosition(ComponentGrid);
+
+            var offsetX = (translateTransform == null ? componentPosition.Value.X : componentPosition.Value.X - translateTransform.X) + deltaX - mousePosition.X;
+            var offsetY = (translateTransform == null ? componentPosition.Value.Y : componentPosition.Value.Y - translateTransform.Y) + deltaY - mousePosition.Y;
+
+            this.SampleComponent.RenderTransform = new TranslateTransform(-offsetX, -offsetY);
         }
     }
 }
