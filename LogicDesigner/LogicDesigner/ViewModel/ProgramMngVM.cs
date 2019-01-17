f1cc5b566d539e34c8df2c42275756e610f7f7eb
Ekaterina Lisyanskaya <102575@fhwn.ac.at>
2019-01-17 15:28:53 +0100

    ViewModel added

---
+++ LogicDesigner/LogicDesigner/ViewModel/ProgramMngVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicDesigner.ViewModel
+{
+    public class ProgramMngVM
+    {
+        private ObservableCollection<ComponentVM> nodesVMInField;
+        private ObservableCollection<ComponentVM> possibleComponentsToChooseFrom;
+        private Command addComponentToField;
+        private Command removeComponentFromField;
+
+        public ObservableCollection<ComponentVM> PossibleComponentsToChooseFrom
+        {
+            get
+            {
+                return this.possibleComponentsToChooseFrom;
+            }
+        }
+    }
+}
